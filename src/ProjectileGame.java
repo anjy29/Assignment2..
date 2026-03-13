d15eaf6913d96e1988193e3e5439cd116a7eb50f
anjy29 <anjy.anjy2552@gmail.com>
2026-03-13 10:57:23 -0400

    Add Comment header

---
+++ src/ProjectileGame.java
@@ -0,0 +1,9 @@
+/*
+name: Anjy Alassaf
+Date: Mar 9,2026
+Description: A two player projectile game where each player chooses a speed and angle to fire.
+The program calculates where the projectile lands and announces the winner when it hits near the opponent.
+*/
+public class ProjectileGame {
+
+}
